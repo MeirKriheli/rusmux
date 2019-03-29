ab6468f56ad6f324a2304c11edfabbb43362098b
Meir Kriheli <mkriheli@gmail.com>
2019-03-29 18:00:51 +0300

    Initial subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{app_from_crate, Arg};
+use clap::{app_from_crate, Arg, SubCommand, AppSettings};
 
 fn main() {
-    let _ = app_from_crate!()
-        .arg(Arg::with_name("COMMAND")
-                .help("Command or project name to execute")
-                .required(true)
-             )
+    let matches = app_from_crate!()
+        .setting(AppSettings::SubcommandsNegateReqs)
+        .arg(Arg::with_name("command")
+                    .help("Subcommand or project name")
+                    .required(true)
+                )
+        .subcommand(SubCommand::with_name("list")
+                       .about("List all projects"))
         .get_matches();
+
+    if let Some(project_name) = matches.value_of("command") {
+        println!("{} project", project_name);
+    }
+    else {
+        match matches.subcommand_name() {
+            Some("list") => println!("Available projects"),
+            None => println!("Please specify a command"),
+            _ => println!("TODO run the project"),
+        }
+    }
 }
