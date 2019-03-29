6e1902b20068ddd01c3dc4be45c92e7ac1e29743
Meir Kriheli <mkriheli@gmail.com>
2019-03-29 19:13:12 +0300

    Implement `list` command

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,47 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{app_from_crate, crate_name, crate_authors, Arg, SubCommand, AppSettings};
-use app_dirs::{AppInfo, AppDataType, app_root};
+use app_dirs::{app_root, AppDataType, AppInfo};
+use clap::{app_from_crate, crate_authors, crate_name, AppSettings, Arg, SubCommand};
+use glob::glob;
+use std::path::Path;
 
-const APP_INFO: AppInfo = AppInfo{
+const APP_INFO: AppInfo = AppInfo {
     name: crate_name!(),
-    author: crate_authors!()
+    author: crate_authors!(),
 };
-
 
 fn list_projects() {
     let config_dir = app_root(AppDataType::UserConfig, &APP_INFO).unwrap();
-    println!("Available projects at {:?}", config_dir);
+
+    let pattern = Path::new(&config_dir)
+        .join("*.yml")
+        .into_os_string()
+        .into_string()
+        .unwrap();
+
+    for project in glob(&pattern).expect("Failed to glob config dir") {
+        match project {
+           Ok(path) =>  println!("{}", Path::new(&path).file_stem().unwrap().to_str().unwrap()),
+           Err(e) => println!("{:?}", e),
+        }
+    }
 }
 
 fn main() {
     let matches = app_from_crate!()
         .setting(AppSettings::SubcommandsNegateReqs)
-        .arg(Arg::with_name("command")
-                    .help("Subcommand or project name")
-                    .required(true)
-                )
-        .subcommand(SubCommand::with_name("list")
-                       .about("List all projects"))
+        .arg(
+            Arg::with_name("command")
+                .help("Subcommand or project name")
+                .required(true),
+        )
+        .subcommand(SubCommand::with_name("list").about("List all projects"))
         .get_matches();
 
     if let Some(project_name) = matches.value_of("command") {
         println!("{} project", project_name);
-    }
-    else {
+    } else {
         match matches.subcommand_name() {
             Some("list") => list_projects(),
             None => println!("Please specify a command"),
