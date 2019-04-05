91905c88f4ee33f04382a114f5b463b58a5acaaa
Meir Kriheli <mkriheli@gmail.com>
2019-04-05 17:36:03 +0300

    Parse window panes and layout

--- src/project.rs
+++ src/project.rs
@@ -1,4 +1,4 @@
-use yaml_rust::yaml::Yaml;
+use yaml_rust::yaml::{Yaml, YamlLoader};
 
 #[derive(Debug)]
 pub struct Project {
@@ -23,8 +23,11 @@ impl Project {
 
         let windows = match hash.get(&Yaml::from_str("windows")) {
             Some(c) => c.to_owned().into_iter().map(|x| Window::new(&x)).collect(),
-            _ => Vec::new(),
+            _ => vec![Window::new(
+                &YamlLoader::load_from_str("default: #").unwrap()[0],
+            )],
         };
+
         Project {
             name: hash[&Yaml::from_str("project_name")]
                 .to_owned()
@@ -44,13 +47,30 @@ impl Project {
 #[derive(Debug)]
 struct Window {
     name: String,
+    panes: Vec<Option<String>>,
+    layout: Option<String>,
 }
 
 impl Window {
     pub fn new(window: &Yaml) -> Self {
         let (name, content) = window.as_hash().unwrap().iter().next().unwrap();
+
+        let (panes, layout) = match content {
+            Yaml::String(cmd) => (vec![Some(cmd.to_owned())], None),
+            Yaml::Hash(config) => (
+                match config.get(&Yaml::from_str("panes")) {
+                    Some(Yaml::Array(cmds)) => cmds.into_iter().map(|x| x.to_owned().into_string()).collect(),
+                    _ => vec![],
+                },
+                config.get(&Yaml::from_str("layout")).unwrap().to_owned().into_string()
+            ),
+            _ => (vec![], None),
+        };
+
         Window {
             name: name.to_owned().into_string().unwrap(),
+            panes: panes,
+            layout: layout,
         }
     }
 }
