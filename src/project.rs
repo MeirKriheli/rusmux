f0777f1d9ee8a40b3a02ac4f5f2e8398abd1ed0d
Meir Kriheli <mkriheli@gmail.com>
2019-04-16 01:55:04 +0300

    DRY getting values, optional project name and root

--- src/project.rs
+++ src/project.rs
@@ -1,25 +1,24 @@
-use yaml_rust::yaml::{Yaml, YamlLoader};
+use yaml_rust::yaml::{Hash, Yaml, YamlLoader};
 
 #[derive(Debug)]
 pub struct Project {
-    name: String,
-    root: String,
+    name: Option<String>,
+    root: Option<String>,
     pre: Option<String>,
     pre_window: Option<String>,
     windows: Vec<Window>,
 }
 
+fn get_value(hash: &Hash, key: &str) -> Option<String> {
+    match hash.get(&Yaml::from_str(key)) {
+        Some(p) => p.to_owned().into_string(),
+        _ => None,
+    }
+}
+
 impl Project {
     pub fn new(project: &Yaml) -> Self {
         let hash = project.as_hash().unwrap();
-        let pre = match hash.get(&Yaml::from_str("pre")) {
-            Some(p) => p.to_owned().into_string(),
-            _ => None,
-        };
-        let pre_window = match hash.get(&Yaml::from_str("pre_window")) {
-            Some(c) => c.to_owned().into_string(),
-            _ => None,
-        };
 
         let windows = match hash.get(&Yaml::from_str("windows")) {
             Some(c) => c.to_owned().into_iter().map(|x| Window::new(&x)).collect(),
@@ -29,16 +28,10 @@ impl Project {
         };
 
         Project {
-            name: hash[&Yaml::from_str("project_name")]
-                .to_owned()
-                .into_string()
-                .unwrap(),
-            root: hash[&Yaml::from_str("project_root")]
-                .to_owned()
-                .into_string()
-                .unwrap(),
-            pre: pre,
-            pre_window: pre_window,
+            name: get_value(&hash, &"project_name"),
+            root: get_value(&hash, &"project_root"),
+            pre: get_value(&hash, &"pre"),
+            pre_window: get_value(&hash, &"pre_window"),
             windows: windows,
         }
     }
@@ -59,10 +52,13 @@ impl Window {
             Yaml::String(cmd) => (vec![Some(cmd.to_owned())], None),
             Yaml::Hash(config) => (
                 match config.get(&Yaml::from_str("panes")) {
-                    Some(Yaml::Array(cmds)) => cmds.into_iter().map(|x| x.to_owned().into_string()).collect(),
+                    Some(Yaml::Array(cmds)) => cmds
+                        .into_iter()
+                        .map(|x| x.to_owned().into_string())
+                        .collect(),
                     _ => vec![],
                 },
-                config.get(&Yaml::from_str("layout")).unwrap().to_owned().into_string()
+                get_value(&config, &"layout"),
             ),
             _ => (vec![], None),
         };
