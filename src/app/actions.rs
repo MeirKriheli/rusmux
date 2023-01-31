886ce484a599e671cb13f57719dc30bd0f8340f7
Meir Kriheli <mkriheli@gmail.com>
2023-01-31 13:37:56 +0200

    Moved the program's actions to a module under `app`

---
+++ src/app/actions.rs
@@ -0,0 +1,70 @@
+use super::config;
+use crate::{
+    error::AppError,
+    project_config::ProjectConfig,
+    tmux::{self, TmuxProject},
+};
+use glob::glob;
+use std::env;
+use std::path::Path;
+use which::which;
+
+/// List the project files in the configuration directory
+pub(crate) fn list_projects() -> Result<(), AppError> {
+    let pattern = config::get_path("*.yml")?;
+
+    for project in glob(&pattern).expect("Failed to glob config dir") {
+        match project {
+            Ok(path) => println!(
+                "{}",
+                Path::new(&path).file_stem().unwrap().to_str().unwrap()
+            ),
+            Err(e) => println!("{e:?}"),
+        }
+    }
+
+    Ok(())
+}
+
+// Parses the project file, prints shell commands
+pub(crate) fn debug_project(project_name: &str) -> Result<(), AppError> {
+    let entries = config::get_project_yaml(project_name)?;
+    let project = ProjectConfig::try_from(entries)?;
+    let tmux = TmuxProject::new(&project)?;
+    println!("{tmux}");
+    Ok(())
+}
+
+/// Parses the project file, creates the tmux session
+pub fn run_project(project_name: &str) -> Result<(), AppError> {
+    println!("Starting project {project_name}");
+
+    let entries = config::get_project_yaml(project_name)?;
+    let project = ProjectConfig::try_from(entries)?;
+    let tmux = TmuxProject::new(&project)?;
+    tmux.run()
+}
+
+fn bool_to_yesno(val: bool) -> &'static str {
+    if val {
+        "Yes"
+    } else {
+        "No"
+    }
+}
+
+// Check the configuration
+pub(crate) fn check_config() -> Result<(), AppError> {
+    let have_tmux = which(tmux::TMUX_BIN).is_ok();
+    let have_editor = env::var("EDITOR").is_ok();
+    let have_shell = env::var("SHELL").is_ok();
+
+    println!(
+        "tmux is installed? {}\n$EDITOR is set? {}\n$SHELL is set? {}",
+        bool_to_yesno(have_tmux),
+        bool_to_yesno(have_editor),
+        bool_to_yesno(have_shell)
+    );
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -6,76 +6,9 @@ mod error;
 mod project_config;
 mod tmux;
 
+use app::actions;
 use clap::{command, Arg, Command};
-use app::config;
 use error::AppError;
-use glob::glob;
-use project_config::ProjectConfig;
-use std::convert::TryFrom;
-use std::env;
-use std::path::Path;
-use tmux::TmuxProject;
-use which::which;
-
-/// List the project files in the configuration directory
-fn list_projects() -> Result<(), AppError> {
-    let pattern = config::get_path("*.yml")?;
-
-    for project in glob(&pattern).expect("Failed to glob config dir") {
-        match project {
-            Ok(path) => println!(
-                "{}",
-                Path::new(&path).file_stem().unwrap().to_str().unwrap()
-            ),
-            Err(e) => println!("{e:?}"),
-        }
-    }
-
-    Ok(())
-}
-
-// Parses the project file, creates the tmux session
-fn run_project(project_name: &str) -> Result<(), AppError> {
-    println!("Starting project {project_name}");
-
-    let entries = config::get_project_yaml(project_name)?;
-    let project = ProjectConfig::try_from(entries)?;
-    let tmux = TmuxProject::new(&project)?;
-    tmux.run()
-}
-
-// Parses the project file, prints shell commands
-fn debug_project(project_name: &str) -> Result<(), AppError> {
-    let entries = config::get_project_yaml(project_name)?;
-    let project = ProjectConfig::try_from(entries)?;
-    let tmux = TmuxProject::new(&project)?;
-    println!("{tmux}");
-    Ok(())
-}
-
-fn bool_to_yesno(val: bool) -> &'static str {
-    if val {
-        "Yes"
-    } else {
-        "No"
-    }
-}
-
-// Check the configuration
-fn check_config() -> Result<(), AppError> {
-    let have_tmux = which(tmux::TMUX_BIN).is_ok();
-    let have_editor = env::var("EDITOR").is_ok();
-    let have_shell = env::var("SHELL").is_ok();
-
-    println!(
-        "tmux is installed? {}\n$EDITOR is set? {}\n$SHELL is set? {}",
-        bool_to_yesno(have_tmux),
-        bool_to_yesno(have_editor),
-        bool_to_yesno(have_shell)
-    );
-
-    Ok(())
-}
 
 fn main() -> Result<(), AppError> {
     let matches = command!()
@@ -97,17 +30,17 @@ fn main() -> Result<(), AppError> {
         .get_matches();
 
     if let Some(project_name) = matches.get_one::<String>("project") {
-        return run_project(project_name);
+        return actions::run_project(project_name);
     }
 
     match matches.subcommand() {
-        Some(("list", _)) => list_projects(),
-        Some(("doctor", _)) => check_config(),
+        Some(("list", _)) => actions::list_projects(),
+        Some(("doctor", _)) => actions::check_config(),
         Some(("debug", debug_matches)) => {
-            debug_project(debug_matches.get_one::<String>("project").unwrap())
+            actions::debug_project(debug_matches.get_one::<String>("project").unwrap())
         }
         Some(("run", run_matches)) => {
-            run_project(run_matches.get_one::<String>("project").unwrap())
+            actions::run_project(run_matches.get_one::<String>("project").unwrap())
         }
         _ => unreachable!(),
     }
